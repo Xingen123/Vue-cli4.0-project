d34559f3996eda6cc17cea60726021c4d6c40815
xingen <335728491@qq.com>
2020-11-27 15:37:12 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,18 @@
   <div id="app">
     <img
       alt="Vue logo"
-      src="./assets/logo.png"
+      src="./assets/logo.jpg"
     />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div class="app_img">
-      <div>
-        dddd
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3306/'
-axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  mounted () {
-    axios({
-      method: 'GET',
-      url: 'api/user',
-      params: {},
-      dataType: 'json'
-    }).then((res) => {
-      console.log(res)
-    })
-  }
-}
+
 </script>
 
 <style lang="scss">
-@import './styles/mixin.scss';
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.app_img {
-  width: 100px;
-  height: 200px;
-  border: 1px solid red;
-  div {
-    @include multi-ellipsis();
-  }
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
